d242f37789e3917cdba6d6012b579d33c76050f2
root <root@ip-172-31-43-156.ec2.internal>
2020-08-18 07:04:40 +0000

    subido

--- vistas/modelos/conexion.php
+++ vistas/modelos/conexion.php
@@ -5,7 +5,7 @@ class Conexion{
 	public function conectar(){
 
 		$link = new PDO("mysql:host=127.0.0.1;dbname=kardex",
-			            "root",
+			            "joel",
 			            "12345678");
 
 		$link->exec("set names utf8");
